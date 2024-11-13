89823095d45208f90231a83b695638419d16b58c
AljazR <aljoz.rakovec@gmail.com>
2024-11-13 19:18:10 +0100

    Update instructions

--- apple_format_converter.py
+++ apple_format_converter.py
@@ -2,11 +2,11 @@
 # All converted files are saved in a new directory, including other files that are not converted.
 
 # Usage:
-# 1. Change directory and new_directory variables to the desired paths
-# 2. Run the script with: python3 heic_converter.py
-# or
 # 1. Run the script with: python3 heic_converter.py
 # 2. Enter the path to the directory containing media files you want to convert.
+# or
+# 1. Change directory and new_directory variables to the desired paths
+# 2. Run the script with: python3 heic_converter.py
 
 # Requirements:
 # Pillow: pip install Pillow
